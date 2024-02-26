dd29b50594cbd8efaf9c8cd2255f4c00d3b61048
im-alok <alok.ranjan.aiml25@heritageit.edu.in>
2024-02-26 23:58:11 +0530

    COncept of euler tree and pre in and post concept

--- Advance_Java/Recursion/ZigZag.java
+++ Advance_Java/Recursion/ZigZag.java
@@ -1,5 +1,24 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class ZigZag {
-    
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==0)
+            return;
+        System.out.print(n);
+        pzz(n-1);
+        System.out.print(n);
+        pzz(n-1);
+        System.out.print(n);
+    }
 }
+
+//concept of euler tree and pre in and post concept of recursion
